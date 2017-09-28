80620f398794f36ddd02dcc37ef84927f5d185cb
VladislavBaranov <31881026+VladislavBaranov@users.noreply.github.com>
2017-09-29 00:29:52 +0300

    Update tic-tac-toe.js

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,14 +1,22 @@
 class TicTacToe {
     constructor() {
-
+        this.currentPlayer = 'x';
+        this.matrix = [
+            [null, null, null],
+            [null, null, null],
+            [null, null, null]
+        ];
     }
 
     getCurrentPlayerSymbol() {
-
+        return this.currentPlayer;
     }
 
     nextTurn(rowIndex, columnIndex) {
-
+        if (!this.matrix[rowIndex][columnIndex]) {
+            this.matrix[rowIndex][columnIndex] = this.currentPlayer;
+            this.currentPlayer = (this.currentPlayer === 'x' ? 'o' : 'x');
+        }
     }
 
     isFinished() {
@@ -16,11 +24,15 @@ class TicTacToe {
     }
 
     getWinner() {
-
+        
     }
 
     noMoreTurns() {
-
+        let result = 0;
+        this.matrix.forEach(row => {
+            result += row.filter(item => item === null).length;
+        });
+        return !result;
     }
 
     isDraw() {
@@ -28,7 +40,7 @@ class TicTacToe {
     }
 
     getFieldValue(rowIndex, colIndex) {
-
+        return this.matrix[rowIndex][colIndex];
     }
 }
 
